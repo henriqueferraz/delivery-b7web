5548005c5221bb0aacfe52a6b9f3d808f06b8691
Henrique Ferraz <henriqueferraz@ofnet.com.br>
2024-06-18 18:45:25 -0300

    final

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <div>
-        ... Página Inicial ...
+        ... Página Inicial ...<br />
+        Clique para para acessar o<Link href='/b7burger/login'><strong> B7 - BURGER </strong></Link><br />
+        Clique aqui para acessar o<Link href='/b7pizza/login'><strong> B7 - Pizza </strong></Link><br />
+        para logar <br />
+        usuário: teste@teste.com.br<br />
+        senha: teste<br />
       </div>
     </>
   );
